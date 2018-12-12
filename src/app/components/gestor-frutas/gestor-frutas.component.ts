bc5b7176529a9966aec8fdc676cfac9a05b4b4d4
Curso <Curso@IPARTEKAULA.COM>
2018-12-12 10:53:28 +0100

    Buscador y filtro en listado gestor de frutas

--- src/app/components/gestor-frutas/gestor-frutas.component.ts
+++ src/app/components/gestor-frutas/gestor-frutas.component.ts
@@ -12,9 +12,16 @@ import { Router } from '@angular/router';
 export class GestorFrutasComponent implements OnInit {
 
   frutas: Fruta[];
+  txtBusqueda: string;
+  frutasBusqueda: Fruta[];
+  filtro: boolean;
 
   constructor(public frutaService: FrutaService, private loginService: LoginService, private router: Router) { 
     console.trace('GestorFrutasComponent constructor');
+
+    this.txtBusqueda = "";
+    this.frutasBusqueda = [];
+    this.filtro = undefined;
   }
 
   ngOnInit() {
@@ -41,6 +48,23 @@ export class GestorFrutasComponent implements OnInit {
     console.trace('AppComponent logout');
     this.loginService.logout();
     this.router.navigate(['login']);
+  }
+
+  buscar(): void {
+    this.frutasBusqueda = [];
+    if (this.txtBusqueda == "") {
+      this.recargarLista();
+    } else {
+      this.frutas.forEach(e => {
+        if (e.nombre.toLowerCase().includes( this.txtBusqueda.toLowerCase() )) {
+          this.frutasBusqueda.push(e);
+        }
+      });
+      this.frutas = this.frutasBusqueda;
+
+    }
+    
+    console.log(this.frutasBusqueda.length);
   }
 
 }
